4af1c0755d17522af55eaf41c99da8ec82b11f86
Pieter <162095334+ST10434048@users.noreply.github.com>
2024-04-17 10:26:26 +0200

    Update

--- src/main/java/algs/Bubble.java
+++ src/main/java/algs/Bubble.java
@@ -2,10 +2,15 @@ package algs;
 
 public class Bubble {
     public static int[] sort(int[] _random_array){
-//        for(int i = 0;i <_random_array.length; i++ ){
-//
-//        }
-
-        return _random_array;
+        for(int j = 0;j <_random_array.length; j++ ){
+            for(int i = 0;i <_random_array.length-1-j; i++ ){
+                if(_random_array[i]> _random_array[i+1] ){
+                    int temp = _random_array[i+1];
+                    _random_array[i+1] = _random_array[i]
+                    _random_array[i]=temp;
+                }
+            }
+            return _random_array;
+        }
     }
 }
